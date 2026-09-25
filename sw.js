33f29dcee4c3ea1fac99fb12c2aa6bac2fadd85e
kaloveza12 <Siriwat.business@gmail.com>
2026-09-25 19:21:40 +0700

    Add pork flavor variants and refresh homepage price card

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bmf-pwa-v1';
+const CACHE='bmf-pwa-v2';
 const SHELL=['/','/index.html','/order/','/order/index.html','/order/products.js','/manifest.webmanifest','/pwa-install.js','/icons/icon-192.png','/icons/icon-512.png','/order/images/bmf-bear-transparent.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
